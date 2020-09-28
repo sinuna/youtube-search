f69930bee2e7b1fbccf20a946266161f92257573
sinuna <sinuna.chaudhary@gmail.com>
2020-09-28 15:36:58 -0400

    Inital Commit

---
+++ build/precache-manifest.8c9732f724280b1458b8533632593a8e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ab9519ace627a26a2bb22e05965ccbeb",
+    "url": "/index.html"
+  },
+  {
+    "revision": "77a18b5526597fe67825",
+    "url": "/static/css/main.29f23181.chunk.css"
+  },
+  {
+    "revision": "5ae8b8b3e0e489527d0f",
+    "url": "/static/js/2.bf3290fd.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.bf3290fd.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "77a18b5526597fe67825",
+    "url": "/static/js/main.30b48d3e.chunk.js"
+  },
+  {
+    "revision": "08d51f09ba28e91188e6",
+    "url": "/static/js/runtime-main.f8447e4a.js"
+  }
+]);
